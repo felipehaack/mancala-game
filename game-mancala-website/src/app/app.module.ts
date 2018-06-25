476995778510163bf23431f61dfe373b9d5212b5
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2018-06-25 10:36:53 +0200

    added app module for all imports in the project, such as: routes, services, provides

---
+++ game-mancala-website/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+
+import {AppComponent} from './app.component'
+import {CardComponent, CardComponentEmitter} from './card/card.component'
+import {BoardComponent, BoardComponentEmitter} from './board/board.component'
+import {RouterModule, Routes} from '@angular/router'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './home/home.component'
+import {HttpClientModule} from '@angular/common/http'
+import {MatButtonModule, MatSelectModule} from '@angular/material';
+
+const appRoutes: Routes = [
+  {
+    path: 'board/:id',
+    component: BoardComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+]
+
+export const rooting = RouterModule.forRoot(appRoutes)
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    BoardComponent,
+    HomeComponent
+  ],
+  imports: [
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    MatSelectModule,
+    MatButtonModule,
+    rooting
+  ],
+  providers: [
+    CardComponentEmitter,
+    BoardComponentEmitter
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {
+}
