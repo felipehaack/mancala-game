a6ee346125872ee1875cce8a95ccaaac7be59fa9
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2018-06-25 13:11:34 +0200

    removed semicolon

--- game-mancala-website/src/app/app.module.ts
+++ game-mancala-website/src/app/app.module.ts
@@ -6,10 +6,10 @@ import {CardComponent, CardComponentEmitter} from './card/card.component'
 import {BoardComponent, BoardComponentEmitter} from './board/board.component'
 import {RouterModule, Routes} from '@angular/router'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {HomeComponent} from './home/home.component'
 import {HttpClientModule} from '@angular/common/http'
-import {MatButtonModule, MatSelectModule} from '@angular/material';
+import {MatButtonModule, MatSelectModule} from '@angular/material'
 
 const appRoutes: Routes = [
   {
